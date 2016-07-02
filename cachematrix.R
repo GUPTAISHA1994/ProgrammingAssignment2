469df81a38527e87a3173bcb51308937621d40d5
GUPTAISHA1994 <ge14isha@mse.ac.in>
2016-07-02 18:27:02 +0530

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The below function cache the time consuming calculations and thus preventing R from recalculating the same thinf again and again.
 
-## Write a short comment describing this function
+## The function generates a special matrix whose inverse can be cached.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+             inv<- NULL
+             set<-function(y)   {
+                   x <<- y
+                  inv <<- NULL
+              }
+              get <- function() x
+        setinv = function(inverse) inv <<- inverse 
+        getinv = function() inv
+        list(set=set, get=get, 
+             setinv=setinv, 
+             getinv=getinv)
 }
 
 
-## Write a short comment describing this function
+## This function returns inverse of the special matrix created by the above function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinv()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinv(inv)
+        return(inv)
 }
+
+
+
